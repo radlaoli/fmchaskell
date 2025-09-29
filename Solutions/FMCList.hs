a0fc92e6278a4da14f1248e7f0293f1da56108a6
Radla Oliveira <radlaoliveira5@gmail.com>
2025-09-29 20:04:15 -0300

    Defini as funções da FMCList até a snoc

--- Solutions/FMCList.hs
+++ Solutions/FMCList.hs
@@ -57,29 +57,38 @@ write [u,v]     for our u `Cons` (v `Cons` Nil)
 
 -}
 
-head :: [a] -> a
-head = undefined
+head :: [a] -> a 
+head [] = error "empty list"
+head (x:xs) = x 
 
 tail :: [a] -> [a]
-tail = undefined
+tail []= error "empty list"
+tail (x:xs) = xs
 
 null :: [a] -> Bool
-null = undefined
+null [] = True
+null _ = False 
 
 length :: Integral i => [a] -> i
-length = undefined
+length [] = 0
+lenght (x:xs) = 1 + lenght xs 
 
 sum :: Num a => [a] -> a
-sum = undefined
+sum [] = 0
+sum (x:xs) = x + sum xs
 
 product :: Num a => [a] -> a
-product = undefined
+product [] = 0
+product (x:xs) = x * product xs 
 
 reverse :: [a] -> [a]
-reverse = undefined
+reverse [] = []
+reverse (x:xs) = reverse xs ++ [x]
+
 
 (++) :: [a] -> [a] -> [a]
-(++) = undefined
+(++) [] xs = xs
+(++) (x:xs) ys = x : (xs++ ys)
 
 -- right-associative for performance!
 -- (what?!)
@@ -87,7 +96,7 @@ infixr 5 ++
 
 -- (snoc is cons written backwards)
 snoc :: a -> [a] -> [a]
-snoc = undefined
+snoc x xs= xs ++ [x] 
 
 (<:) :: [a] -> a -> [a]
 (<:) = flip snoc
